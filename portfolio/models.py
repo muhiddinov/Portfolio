4fcef8eda21ccbd591323ca6805ccfaca2d0a696
muhiddinov <muhiddinovabulqosim@gmail.com>
2024-06-25 22:50:49 +0500

    Post method updated

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Contact (models.Model):
+    name = models.CharField(max_length=255, null=False)
+    email = models.EmailField(max_length=255, null=False)
+    service5 = models.IntegerField(null=True, default=0)
+    service1 = models.IntegerField(null=True, default=0)
+    service2 = models.IntegerField(null=True, default=0)
+    service3 = models.IntegerField(null=True, default=0)
+    service4 = models.IntegerField(null=True, default=0)
+    message = models.TextField(null=False)
+    createDateTime = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return str(self.email)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from .models import Contact
 
 def index_view (request):
+    if (request.method == "POST"):
+        contact = Contact()
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        service1 = request.POST.get('electronics')
+        service2 = request.POST.get('iot')
+        service3 = request.POST.get('robot')
+        service4 = request.POST.get('drone')
+        service5 = request.POST.get('desktop')
+        contact.name = name
+        contact.email = email
+        contact.message = message
+        contact.service1 = service1
+        contact.service2 = service2
+        contact.service3 = service3
+        contact.service4 = service4
+        contact.service5 = service5
+        contact.save()
     return render(request, 'index.html')
